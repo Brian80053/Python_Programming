14a932bba6e545330c3b7ee0f4229e28ed325839
Brian80053 <imiyeong743@gmail.com>
2026-08-20 12:22:31 +0900

    test

--- 01_datatype/ex1.py
+++ 01_datatype/ex1.py
@@ -11,8 +11,19 @@ print (a,b)
 
 a = b = c = 0
 
-#값 swap
+# 값 swap
 a,b = 2,3
 a,b = b,a
 
 print(a,b)
+
+# 변수명 규칙 (C와 동일)
+# 알파벳, 숫자, 특수문자 (_)만 가능
+# 숫자로 시작 불가
+# 예약어 사용 불가
+# snake_case
+# camelCase
+# strHungarianCase
+# !name = "pororo"
+# 2name = "pororo"
+#class = "test"
