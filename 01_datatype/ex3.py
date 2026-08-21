f27096a3cd8e326f9eb087f512d3e6122d9ea593
Brian80053 <imiyeong743@gmail.com>
2026-08-21 10:27:20 +0900

    exe3.py 올림

---
+++ 01_datatype/ex3.py
@@ -0,0 +1,20 @@
+#불리언
+# True of False
+a=True
+print(a,type(a))
+
+print(1<0)
+print(1>0)
+print(1==0)
+
+print("apple" > "apble")
+
+print(bool(0))
+print(bool(""))
+print(bool([]))
+
+#None 자료형
+a = None
+print(a,type(a))
+print(bool(a))
+
